70409bbf8761eb76446f98958b7887afda99e7fc
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-14 16:24:45 +0200

    Inizio esercizio

---
+++ myScript.js
@@ -0,0 +1,12 @@
+var parolautente;
+
+parolautente = prompt("Inserisci parola palindroma da verificare")
+
+
+function isPalindroma() {
+    var corrispondenza;
+    while(){
+
+    }
+    return true;
+}
